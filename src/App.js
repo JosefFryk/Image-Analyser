899abc1c9c23f7a9dd8fb8b7f4ccd17d8209d972
Josef Fryk <josef.fryk@gmail.com>
2022-04-29 18:40:36 +0200

    0.0.1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,125 @@
-import logo from './logo.svg';
 import './App.css';
+const express = require("express");
+const app = express();
+const multer = require("multer");
+const upload = multer({
+  storage: multer.diskStorage({}),
+  
+  destination: function (req, file, cd) {
+    if (file.fieldname === 'emotion-upload') {
+        cd(null, '/');
+    }
+
+    else if (file.fieldname === 'image-upload') {
+        cd(null, '/');
+    }
+    
+},
+
+filename: function (req, file, cd) {
+  cd(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname));
+},
+
+  fileFilter: (req, file, cb) => {
+    let ext = path.extname(file.originalname);
+    if (ext !== ".jpg" && ext !== ".jpeg" && ext !== ".png") {
+      cb(new Error("File type is not supported"), false);
+      return;
+    }
+    cb(null, true);
+  },
+});
+
+//MS Specific
+const axios = require("axios").default;
+const async = require("async");
+const fs = require("fs");
+const https = require("https");
+const path = require("path");
+const createReadStream = require("fs").createReadStream;
+const sleep = require("util").promisify(setTimeout);
+const ComputerVisionClient =
+  require("@azure/cognitiveservices-computervision").ComputerVisionClient;
+const ApiKeyCredentials = require("@azure/ms-rest-js").ApiKeyCredentials;
+
+require("dotenv").config({ path: "./config/.env" });
+
+const cloudinary = require("cloudinary").v2;
+
+cloudinary.config({
+  cloud_name: process.env.CLOUD_NAME,
+  api_key: process.env.CLOUD_API_KEY,
+  api_secret: process.env.CLOUD_API_SECRET,
+});
+
+const key = process.env.MS_COMPUTER_VISION_SUBSCRIPTION_KEY;
+const endpoint =  process.env.MS_COMPUTER_VISION_ENDPOINT;
+
+const computerVisionClient = new ComputerVisionClient(
+  new ApiKeyCredentials({ inHeader: { "Ocp-Apim-Subscription-Key": key } }),
+  endpoint
+);
+
+//Server Setup
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+
+//Routes
+app.get("/", (req, res) => {
+  res.render("index.ejs");
+});
+
+
+////emotion-check/////
+
+
+app.post("/", upload.single("file-to-upload"), async (req, res) => {
+  try {
+    let hotDogCount = 0;
+    // Upload image to cloudinary
+    const result = await cloudinary.uploader.upload(req.file.path);
+    const objectURL = result.secure_url;
+
+    // Analyze a URL image
+    console.log("Analyzing objects in image...", objectURL.split("/").pop());
+
+    const objects = (
+      await computerVisionClient.analyzeImage(objectURL, {
+        visualFeatures: ["Objects"],
+      })
+    ).objects;
+    console.log();
+
+    // Print objects bounding box and confidence
+       function formatTags(object) {
+        return object.map(obj => (`${obj.name} (${obj.confidence.toFixed(2)})`)).join(', ');
+      }
+    
+    function formatRectObjects(rect) {
+      return (
+        `top=${rect.y}`.padEnd(10) +
+        `left=${rect.x}`.padEnd(10) +
+        `bottom=${rect.y + rect.h}`.padEnd(12) +
+        `right=${rect.x + rect.w}`.padEnd(10) +
+        `(${rect.w}x${rect.h})`
+      );
+    }
+
+    res.render("result.ejs", { count: hotDogCount, img: objectURL });
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+app.listen(process.env.PORT || 8000);
+
+
+//////////////////////////////////////////////////
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     
     </div>
   );
 }
